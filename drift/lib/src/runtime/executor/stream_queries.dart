e7c909f0483b84510a9a60da8b020adc82ff16a3
Simon Binder <oss@simonbinder.eu>
2025-09-11 21:43:46 +0200

    Close streams before database

--- drift/lib/src/runtime/executor/stream_queries.dart
+++ drift/lib/src/runtime/executor/stream_queries.dart
@@ -178,7 +178,7 @@ class StreamQueryStore {
     _isShuttingDown = true;
 
     for (final stream in _activeKeyStreams.values) {
-      stream.close();
+      await stream.close();
     }
     // awaiting this is fine - the stream is never exposed to users and we don't
     // pause any subscriptions on it.
@@ -209,7 +209,7 @@ class QueryStream<Rows extends Object> {
       final queryListener = _QueryStreamListener(listener);
 
       if (_isClosed) {
-        listener.closeSync();
+        listener.close();
         return;
       }
 
@@ -358,12 +358,13 @@ class QueryStream<Rows extends Object> {
     }
   }
 
-  void close() {
+  Future<void> close() async {
     _isClosed = true;
-    for (final listener in _listeners) {
-      listener.controller.close();
-    }
+
+    final listenersDone = Future.wait(
+        [for (final listener in _listeners) listener.controller.close()]);
     _listeners.clear();
+    await listenersDone;
   }
 }
 
